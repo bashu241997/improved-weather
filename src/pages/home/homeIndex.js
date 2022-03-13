79d873238ee059eb165f9afea8d0568196b3ab89
bashu241997 <p.baslass@gmail.com>
2022-03-13 11:18:40 +0530

    mixed content

--- src/pages/home/homeIndex.js
+++ src/pages/home/homeIndex.js
@@ -45,6 +45,7 @@ export default function Homeindex() {
     <>
       <Head>
         <title>{title}</title>
+        <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests"></meta>
       </Head>
       <div
         className={`md:container md:mx-auto mx-auto px-4 py-4 `}
